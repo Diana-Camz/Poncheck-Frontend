89be8b2113feaaa57a6136a63349c9d92521f575
Diana-Camz <dcampos0495@gmail.com>
2026-07-17 17:48:43 -0600

    feat: configure tanstack and axios

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,19 +1,26 @@
 "use client";
 
+import { QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '@/utils/styles/theme'
 import { Toaster } from "sonner";
+import { makeQueryClient } from "@/lib/tanstack-query/react-query";
+import { useState } from "react";
 
 export function Providers({
     children
 }: { children: React.ReactNode }) {
+    const [queryClient] = useState(makeQueryClient);
     return (
-        <ThemeProvider theme={theme}>
-            <Toaster/>
-            <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-                {children}
-            </NextThemesProvider>
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+
+            <ThemeProvider theme={theme}>
+                <Toaster />
+                <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+                    {children}
+                </NextThemesProvider>
+            </ThemeProvider>
+        </QueryClientProvider>
     );
 }
