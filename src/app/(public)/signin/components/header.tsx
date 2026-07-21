34bcac7cb821a3099cabbee8e5b02e13fa3e7a92
Diana-Camz <dcampos0495@gmail.com>
2026-07-20 18:52:36 -0600

    refactor: update signin screen

---
+++ src/app/(public)/signin/components/header.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { format } from 'date-fns';
+import { es } from 'date-fns/locale';
+import { ThemeToggle } from "@/components/shared/theme-toggle";
+
+export function Header() {
+    const date = new Date();
+    const formattedDate = format(date, 'PPPP', { locale: es });
+    return (
+        <header className="top-0 z-40 hidden border-b border-border bg-background-2 backdrop-blur md:block">
+            <div className="flex h-13 items-center justify-between gap-4 px-4 lg:px-6">
+                <div>
+                    <Link href="/menu" className="flex items-center gap-2">
+                        <span className="text-3xl font-semibold text-foreground">
+                            PONCHECK POS
+                        </span>
+                    </Link>
+                </div>
+                <div className="lg:w-1/5 flex items-center justify-between ">
+                    <span className="lg:text-lg font-medium mr-4 lg:m-0">{formattedDate}</span>
+                    <ThemeToggle />
+                </div>
+            </div>
+        </header>
+    )
+};
