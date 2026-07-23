67625e66aef21853aef4e241b3e555ecf075096c
Diana-Camz <dcampos0495@gmail.com>
2026-07-22 22:08:34 -0600

    fix(auth): validate expired sessions on private routes

--- src/features/products/types/products.types.ts
+++ src/features/products/types/products.types.ts
@@ -1,19 +1,15 @@
-export type ProductItem = {
-    id: string;
-    name: string;
-    code: string;
-    stock: number;
-    price: number;
-    flavor?: string;
-    description?: string;
-    active: boolean;
-    poncheBase: "WINE" | "MILK" | "WATER" | null;
-    productSize: "SMALL" | "MEDIUM" | "LARGE" | null;
-    category: string;
-}
 
-export type Category = {
-  id: string;
+
+export type Product = {
+  id: number;
   name: string;
+  code: string;
+  stock: number;
+  price: number;
+  flavor?: string;
+  description?: string;
   active: boolean;
-};
+  poncheBase: "MILK" | "WATER" | "WINE" | "MEZCAL" | null;
+  productSize: "SMALL" | "MEDIUM" | "LARGE" | null;
+  category: string;
+}
