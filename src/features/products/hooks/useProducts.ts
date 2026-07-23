2870cd2982e7bbea7ccd3a9ae5f5dbc54ae9b5d8
Diana-Camz <dcampos0495@gmail.com>
2026-07-22 20:46:55 -0600

    refactor: add configurations on cache data

--- src/features/products/hooks/useProducts.ts
+++ src/features/products/hooks/useProducts.ts
@@ -5,5 +5,8 @@ export function useProducts() {
     return useQuery({
         queryKey: ["products"],
         queryFn: getProducts,
+        staleTime: 25 * 60 * 1000,
+        gcTime: 30 * 60 * 1000,
+        refetchOnWindowFocus: false,
     })
 };
--- src/features/users/hooks/useUsers.ts
+++ src/features/users/hooks/useUsers.ts
@@ -16,5 +16,8 @@ export function useCurrentUser() {
         queryKey: ["current-user"],
         queryFn: getCurrentUser,
         retry: false,
+        staleTime: 25 * 60 * 1000,
+        gcTime: 30 * 60 * 1000,
+        refetchOnWindowFocus: false,
     })
 }
